e954cb96382a8fc791a263f9574d7e9fd1a62ac3
whitejh <kei02012@naver.com>
2021-02-01 22:42:55 +0900

    삼각형 판별

---
+++ JSCodingTest/2.삼각형 판별하기.js
@@ -0,0 +1,17 @@
+/* 길이가 서로 다른 a,b,c 세 개의 막대 길이가 주어지면 이 세 막대로 삼각형을 만들 수 있으면
+"YES"를 출력, 만들 수 없으면 "NO"를 출력 */
+
+function solution(a, b, c) {
+  let answer = "YES",
+    max;
+  let sum = a + b + c;
+
+  if (a > b) max = a;
+  else max = b;
+  if (c > max) max = c;
+  if (sum - max <= max) answer = "NO";
+
+  return answer;
+}
+
+console.log(solution(13, 33, 17));
