2da5465d4d41d159b4b48b6f077e9ea332c0ca34
whitejh <kei02012@naver.com>
2021-04-28 23:50:33 +0900

    8.일곱난쟁이

---
+++ JSCodingTest/8.일곱 난쟁이.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+  let answer = arr; // 얕은 복사
+  let sum = arr.reduce((a, b) => a + b, 0);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      if (sum - (arr[i] + arr[j]) === 100) {
+        arr.splice(j, 1);
+        arr.splice(i, 1);
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
+
+// reduce 함수 사용
