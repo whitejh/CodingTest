512619678a8e16878c138cce0955cdc5b5168dc3
whitejh <kei02012@naver.com>
2021-05-04 23:59:38 +0900

    toLowerCase() & toUpperCase()

---
+++ JSCodingTest/12.대문자로통일.js
@@ -0,0 +1,12 @@
+function solution (s){
+  let answer = "";
+  for(let x of s){
+    if (x===x.toLowerCase()) answer+=x.toUpperCase();
+    else answer+=x;
+  }
+  
+  return answer;
+}
+
+  let str="ItisTimeToStudy";
+  console.log(solution(str));
