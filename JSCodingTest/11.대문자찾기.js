a2d5d79fd6d8e040918aedae394ee763bc6d618e
whitejh <kei02012@naver.com>
2021-05-04 23:54:16 +0900

    toUpperCase 메서드

--- JSCodingTest/11.대문자찾기.js
+++ JSCodingTest/11.대문자찾기.js
@@ -1,8 +1,11 @@
 function solution(s) {
   let answer = 0;
   for(let x of s) {
-    if(x===x.toUpperCase()) 
+    if(x===x.toUpperCase()) // 문자를 대문자로 변환
       answer++;
+    
+    //let num=x.charCodeAt();
+    //if(num>= 65 && num <= 90) answer++; //아스키 코드 활용
   }
   return answer;
 }
