8a255b06ae2ab68f99520a7d1efacae37d847922
whitejh <jhk0806@gmail.com>
2021-07-11 18:26:46 +0900

    7/12 1주차

---
+++ Programmers/Lv2/124나라.js
@@ -0,0 +1,26 @@
+function solution(n) {
+    var answer = '';
+
+    while (n > 0) {
+        switch (n % 3) {
+            case 1:
+                answer = '1' + answer;
+                n = Math.floor(n / 3);
+                break;
+            case 2:
+                answer = '2' + answer;
+                n = Math.floor(n / 3);
+                break;
+            case 0:
+                answer = '4' + answer;
+                n = (n / 3) - 1;
+                break;
+        }
+    }
+    return answer;
+}
+
+// 재귀함수 사용
+function solution(n) {
+    return n ? solution(parseInt((n - 1) / 3)) + [1, 2, 4][(n - 1) % 3] : '';
+}
