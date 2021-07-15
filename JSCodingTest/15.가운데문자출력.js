f82c6d98be19c26ba93bac6d8325f1f6ec3502d3
whitejh <jhk0806@gmail.com>
2021-07-15 23:38:13 +0900

    indexOf() 활용

--- JSCodingTest/15.가운데문자출력.js
+++ JSCodingTest/15.가운데문자출력.js
@@ -1,14 +1,10 @@
 function solution(s) {
-  let answer = "";
-
-  if (s.length % 2 === 0) { // 짝수 
-    answer += s.substr(s.length / 2 - 1, 2);
-  } else { // 홀수 
-    answer += s.substr(s.length / 2, 1);
-  }
-
+  let answer;
+  let mid = Math.floor(s.length / 2);
+  if (s.length % 2 == 1) answer = s.substring(mid, mid + 1);
+  else answer = s.substring(mid - 1, mid + 1);
   return answer;
 }
 
-let str = "study";
+let str = "good";
 console.log(solution(str));
