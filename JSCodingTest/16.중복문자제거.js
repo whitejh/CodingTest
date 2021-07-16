85b344473f67c9a02385b8b66121d07f6e73c4bd
whitejh <jhk0806@gmail.com>
2021-07-16 14:23:33 +0900

    중복문자 찾을 때 indexOf() 활용

--- JSCodingTest/16.중복문자제거.js
+++ JSCodingTest/16.중복문자제거.js
@@ -1,10 +1,10 @@
-function solution(s){  
-    let answer="";
-    //console.log(s.indexOf("K"));
-    for(let i=0; i<s.length; i++){
-        //console.log(s[i], i, s.indexOf(s[i]));
-        if(s.indexOf(s[i])===i) answer+=s[i];
-    }
-    return answer;
+//중복문자제거할 때 indexOf() 활용
+function solution(s) {
+  let answer="";
+
+  for(let i=0; i<s.length; i++){
+    if(s.indexOf(s[i])===i) answer+=s[i];
+  }
+  return answer;
 }
-console.log(solution("ksekkset"));
+console.log(solution("ksekkset")); // kset 출력
