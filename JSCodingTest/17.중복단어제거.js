b2bed2a5c121f40278379ec7736b7641ed1deaf7
whitejh <jhk0806@gmail.com>
2021-07-16 14:53:41 +0900

    filter 메서드 활용 & 콜백함수

--- JSCodingTest/17.중복단어제거.js
+++ JSCodingTest/17.중복단어제거.js
@@ -1,8 +1,34 @@
+// filter 메서드 활용
 function solution(s) {
-  let answer = ""; 
- 
+  let answer;
+  answer = s.filter((v, i) => {
+    return s.indexOf(v) === i;
+    // if (s.indexOf(v) === i) return true;
+  });
   return answer;
 }
-
-let str = "StuDY";
+let str = ["good", "time", "good", "time", "student"];
 console.log(solution(str));
+
+// function solution(s) {
+//   let answer =[];
+//   for (let x of s) {
+//     if (answer.indexOf(x) === -1) {
+//       answer += x + '\n';
+//     }
+//   }
+//   return answer;
+// }
+
+// let str = ["5", "good", "time", "good", "time", "student"];
+// console.log(solution(str));
+
+// set 자료구조 활용
+// function solution(s) {
+//   let answer;
+//   answer = Array.from(new Set(str));
+//   return answer;
+// }
+
+// let str = ["good", "time", "good", "time", "student"];
+// console.log(solution(str));
