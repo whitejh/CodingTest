6c88a1ccd864d82df5f047004a30feda669ceca9
whitejh <jhk0806@gmail.com>
2021-07-18 15:30:07 +0900

    7/18 2주차 숫자 문자열과 영단어

--- Programmers/Lv1/숫자 문자열과 영단어.js
+++ Programmers/Lv1/숫자 문자열과 영단어.js
@@ -29,6 +29,7 @@ console.log(solution("123")); // 123
 // g (Global) : 대상 문자열 내에서 패턴과 일치하는 모든 문자열을 전역 검색
 // m (Multi line) : 문자열의 행이 바뀌더라도 패턴 검색을 계속함
 
+
 // 2. string 메서드(split) & 배열 메서드(join) 활용
 function solution(s) {
   let numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
