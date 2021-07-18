53ac3a46ab46768579a517275c662f2ac3affd76
whitejh <jhk0806@gmail.com>
2021-07-18 15:27:47 +0900

    7/18 2주차 소수 만들기

--- Programmers/Lv1/소수 만들기.js
+++ Programmers/Lv1/소수 만들기.js
@@ -1,5 +1,7 @@
 // 프로그래머스 Lv1
 // 소수 만들기
+
+// 1
 function solution(nums) {
   let answer = 0;
   let n = nums.length;
@@ -23,3 +25,30 @@ function isPrime(num) {
   }
   return true; // 소수인 경우
 }
+
+// 2
+function solution(nums)
+{
+  var answer = 0;
+  var array = [];
+  var len = nums.length;
+  for (let i = 0; i < len - 2; i++){
+    for (let j = i + 1; j < len - 1; j++){
+      for (let k = j + 1; k < len; k++){
+        array.push(nums[i] + nums[j] + nums[k]);
+      }
+    }
+  } //reduce함수 이용 
+  answer = array.reduce((acc, cur, idx) => acc + primeNumber(cur), 0)
+  return answer;
+}
+
+//소수 판별 함수 
+function primeNumber(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    //소수가 아닐 경우 
+    if (n % i == 0) { return 0; }
+  } //소수일 경우 
+  return 1;
+}
+
