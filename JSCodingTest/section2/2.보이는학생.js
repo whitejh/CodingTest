bc5f29cc7cac1a2ead25483d1016ff6a88a95aca
whitejh <jhk0806@gmail.com>
2021-07-21 23:41:34 +0900

    1차원 배열 탐색 & 최댓값 활용

---
+++ JSCodingTest/section2/2.보이는학생.js
@@ -0,0 +1,36 @@
+// 1차원 배열 탐색 & 최댓값 활용
+function solution(arr) {
+    let answer = 0, max = arr[0];//max = Number.MIN_SAFE_INTEGER; 
+
+    for (i of arr) {
+        if (i > max) {
+            max = i;
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+const input = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(input));
+
+function solution(arr) {
+    let answer = 0, max = arr[0];//max = Number.MIN_SAFE_INTEGER; 
+
+    for(let i = 1;i < arr.length;i++){
+      if(arr[i] > max) {
+        answer++;
+        max=arr[i];
+        
+      }
+    }
+
+    return answer;
+}
+
+let arr = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr));
+
+
+
