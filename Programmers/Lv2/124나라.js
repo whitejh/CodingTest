772a05240462ce3ad86db52eeacd2e4dfad8a1d4
whitejh <jhk0806@gmail.com>
2021-07-22 23:53:31 +0900

    7/12 1주차 124나라

--- Programmers/Lv2/124나라.js
+++ Programmers/Lv2/124나라.js
@@ -1,27 +1,31 @@
+// 프로그래머스 Lv2
+// 124 나라
+
+// 1. 반복문 & floor()
 function solution(n) {
     var answer = '';
 
     while (n > 0) {
-        switch (n % 3) {
-            case 1:
-                answer = '1' + answer;
-                n = Math.floor(n / 3);
-                break;
-            case 2:
-                answer = '2' + answer;
-                n = Math.floor(n / 3);
-                break;
-            case 0:
-                answer = '4' + answer;
-                n = (n / 3) - 1;
-                break;
+      switch (n % 3) {
+        case 1:
+          answer = '1' + answer;
+          n = Math.floor(n / 3);
+          break;
+        case 2:
+          answer = '2' + answer;
+          n = Math.floor(n / 3);
+          break;
+        case 0:
+          answer = '4' + answer;
+          n = (n / 3) - 1;
+          break;
         }
     }
     return answer;
 }
 console.log(solution(4));
 
-// 재귀함수 사용
+// 2. 재귀함수 사용
 // function solution(n) {
 //     return n ? solution(parseInt((n - 1) / 3)) + [1, 2, 4][(n - 1) % 3] : '';
 // }
