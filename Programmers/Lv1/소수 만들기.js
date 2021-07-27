fb9b735261504de3d03bba732f587e2d03bf73d7
whitejh <jhk0806@gmail.com>
2021-07-27 20:36:42 +0900

    7/27 3주차 키패드 누르기

--- Programmers/Lv1/소수 만들기.js
+++ Programmers/Lv1/소수 만들기.js
@@ -6,8 +6,8 @@ function solution(nums) {
   let answer = 0;
   let n = nums.length;
   
-  for (let i = 0; i < n-2; i++){
-    for (let j = i + 1; j < n-1; j++){
+  for (let i = 0; i < n; i++){
+    for (let j = i + 1; j < n; j++){
       for (let k = j + 1; k < n; k++){
         const number = nums[i] + nums[j] + nums[k];
         if (isPrime(number))
@@ -18,10 +18,9 @@ function solution(nums) {
     return answer;
 }
 
-function isPrime(num) {
-  if (num < 2) return true;
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) return false; // 소수가 아닌 경우
+function isPrime(number) {
+  for (let i = 2; i*i <= number; i++) {
+    if (number % i === 0) return false; // 소수가 아닌 경우
   }
   return true; // 소수인 경우
 }
