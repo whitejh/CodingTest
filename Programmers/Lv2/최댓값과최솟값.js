99f6e1f44d9139ec5190544e74ceb59b3880a370
whitejh <jhk0806@gmail.com>
2021-09-18 15:27:49 +0900

    9/18 10주차

---
+++ Programmers/Lv2/최댓값과최솟값.js
@@ -0,0 +1,30 @@
+// 프로그래머스 Lv2
+// 최댓값과 최솟값
+
+// 1
+function solution(s) {
+  let answer = '';
+  let arr = [];
+  let word = s.split(' ');
+
+  for (let i = 0; i < word.length; i++) {
+    let a = Number(word[i]);
+    arr.push(a);
+  }
+
+  let maxNum = Math.max(...arr);
+  let minNum = Math.min(...arr);
+  let maxString = String(maxNum);
+  let minString = String(minNum);
+
+  answer = minString.concat(' ', maxString);
+
+  return answer;
+}
+
+// 2
+function solution(s) {
+  const arr = s.split(' ');
+
+  return Math.min(...arr) + ' ' + Math.max(...arr);
+}
