ab97e396b6ef42cc8b4a8bd3d59d0561d2401237
whitejh <jhk0806@gmail.com>
2021-09-24 21:42:12 +0900

    9/24 11주차

--- Programmers/Lv2/다리를지나는트럭.js
+++ Programmers/Lv2/다리를지나는트럭.js
@@ -31,3 +31,11 @@ function solution(bridge_length, weight, truck_weights) {
 
   return answer;
 }
+
+// 주어진 bridge_length만큼 길이를 가지는 bridge 배열을 만든다.각 요소는 0으로 초기화한다.
+// while문을 통해서 다리위의 트럭 이동처리, 트럭을 다리위에 올리기 작업을 한다.
+// 다리위의 트럭 이동처리는 bridge의 가장 맨앞 요소를 shift를 사용해서 빼고 가장 뒤에는 push로 0을 넣는다.이 처리는 1초가 지난 것이므로 그만큼 answer에 1초를 더한다.
+// 다리위의 트럭이 이동처리되고 난 뒤에는 bridge에 reduce 메소드를 사용해서 다리위의 전체 요소의 합(다리위의 무게)을 구한다.
+// 그리고 구한 값을 if문을 통해서 다음 트럭이 다리 위에 올 수 있는지 weight와 비교한다.
+// 올라올 수 있으면, truck_weight에 shift 메소드를 사용해서 빼낸 뒤 bridge의 제일 끝 인덱스에 넣고 다리위의 무게에도 더한다.
+// 이 이후에, 다리위 무게가 0이면 이동할 트럭이 더이상 없는 것이므로 break한다
